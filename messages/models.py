6d46ab6e08dd9530583d70a774ec5fad4f4f89fd
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-28 22:47:43 +0500

    chat and message models

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,18 @@
+from datetime import timezone
 from django.db import models
+from chats.models import Chat
+from users.models import User
 
 # Create your models here.
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    content = models.TextField(blank=True, null=True)
+    sent_at = models.DateTimeField(default=timezone.now)
+    is_edited = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    reply_to = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
+
+    def __str__(self):
+        return f"{self.sender.username}: {self.content[:30]}" if self.content else "Media message"
