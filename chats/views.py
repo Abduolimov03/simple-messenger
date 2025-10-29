9612e5144ec1384b5a45f4baea411d212d7148f8
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-29 12:54:17 +0500

    permissions

--- chats/views.py
+++ chats/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .serializers import ChatMemberSerializer, ChatSerializer
 from rest_framework import viewsets
 from .models import ChatMember, Chat
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -10,7 +11,9 @@ from .models import ChatMember, Chat
 class ChatViewSet(viewsets.ModelViewSet):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+    permission_classes = [IsAuthenticated]
 
 class ChatMemberViewset(viewsets.ModelViewSet):
     queryset = ChatMember.objects.all()
     serializer_class = ChatMemberSerializer
+    permission_classes = [IsAuthenticated]
