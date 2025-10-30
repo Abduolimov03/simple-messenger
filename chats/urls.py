99bd060dae49e567fd6d677d6281eec8bb85bfce
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-10-30 10:40:34 +0500

    rename urls

--- chats/urls.py
+++ chats/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ChatMemberViewset, ChatViewSet
 
 router = DefaultRouter()
-router.register(r'messages', ChatMemberViewset, basename='message')
-router.register(r'chat', ChatViewSet, basename='media')
+router.register(r'messages_chat', ChatMemberViewset, basename='message_chat')
+router.register(r'chat_chat', ChatViewSet, basename='media_chat')
 
 urlpatterns = router.urls
