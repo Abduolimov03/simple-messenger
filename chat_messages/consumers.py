bf76259181c9411f92992b5f3ad6853c853a6216
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-10-31 09:30:15 +0500

    update

--- chat_messages/consumers.py
+++ chat_messages/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import User
-from .models import Message
+from .models import Message, MessageStatus
 from django.utils.timezone import now
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -9,7 +9,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        # Kanalga ulanish
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
@@ -17,10 +16,57 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
+
         data = json.loads(text_data)
-        message = data["message"]
-        sender_username = data["sender"]
-        receiver_username = data["receiver"]
+
+        if data.get("event") == "typing_start":
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "typing_event",
+                    "username": data.get("username"),
+                    "status": "start",
+                },
+            )
+            return
+
+        if data.get("event") == "typing_stop":
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "typing_event",
+                    "username": data.get("username"),
+                    "status": "stop",
+                },
+            )
+            return
+
+        if data.get("event") == "mark_read":
+            message_id = data.get("message_id")
+            username = data.get("username")
+            try:
+                user = User.objects.get(username=username)
+                msg_status = MessageStatus.objects.get(message_id=message_id, user=user)
+                msg_status.is_read = True
+                msg_status.read_at = now()
+                msg_status.save()
+
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        "type": "read_update",
+                        "message_id": message_id,
+                        "username": username,
+                        "read_at": msg_status.read_at.strftime("%H:%M:%S"),
+                    },
+                )
+            except (User.DoesNotExist, MessageStatus.DoesNotExist):
+                pass
+            return
+
+        message = data.get("message")
+        sender_username = data.get("sender")
+        receiver_username = data.get("receiver")
 
         await self.save_message(sender_username, receiver_username, message)
 
@@ -32,19 +78,41 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "sender": sender_username,
                 "receiver": receiver_username,
                 "timestamp": now().strftime("%H:%M:%S"),
-            }
+            },
         )
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps(event))
+
+    async def typing_event(self, event):
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "event": "typing",
+                    "username": event["username"],
+                    "status": event["status"],
+                }
+            )
+        )
+
+    async def read_update(self, event):
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "event": "read_update",
+                    "message_id": event["message_id"],
+                    "username": event["username"],
+                    "read_at": event["read_at"],
+                }
+            )
+        )
 
     @staticmethod
     async def save_message(sender_username, receiver_username, message):
         try:
             sender = User.objects.get(username=sender_username)
             receiver = User.objects.get(username=receiver_username)
-            Message.objects.create(
-                sender=sender, receiver=receiver, content=message
-            )
+            msg = Message.objects.create(sender=sender, receiver=receiver, content=message)
+            MessageStatus.objects.create(message=msg, user=receiver)
         except User.DoesNotExist:
             pass
